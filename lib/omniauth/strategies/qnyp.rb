c98b1f47842227aeb8126ea8fafc483392619f16
Junya Ogura <junyaogura@gmail.com>
2017-04-26 21:13:15 +0900

    Improve portability

--- lib/omniauth/strategies/qnyp.rb
+++ lib/omniauth/strategies/qnyp.rb
@@ -2,14 +2,19 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
+    # OmniAuth strategy for qnyp.com
     class Qnyp < OmniAuth::Strategies::OAuth2
-      option :name, 'qnyp'
-      option :scope, ''
+      API_URL = ENV['QNYP_API_URL'] || 'https://api.qnyp.com'
+      SITE_URL = ENV['QNYP_SITE_URL'] || 'https://qnyp.com'
 
-      option :client_options, {
-        site: 'https://api.qnyp.com',
-        authorize_url: 'https://qnyp.com/oauth/authorize',
-      }
+      option :scope, 'public'
+
+      option(
+        :client_options,
+        site: API_URL,
+        authorize_url: "#{SITE_URL}/oauth/authorize",
+        token_url: "#{API_URL}/oauth/token"
+      )
 
       uid { raw_info['id'] }
 
@@ -22,13 +27,15 @@ module OmniAuth
         }
       end
 
-      def raw_info
-        @raw_info ||= access_token.get('/me.json').parsed
-      end
+      private
 
       # https://github.com/intridea/omniauth-oauth2/issues/81
       def callback_url
         full_host + script_name + callback_path
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/me.json').parsed
       end
     end
   end
